1e52c0e625c31e4ed7c3560efe921148f4f6c977
JamieBirch <jamie.d.birch@gmail.com>
2024-04-26 14:44:03 +0200

    add patrolling enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,14 +2,52 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed;
+    
     private bool isAlive = true;
     public GameObject spriteAlive;
     public GameObject spriteDead;
     
+    public Transform[] patrolWaypoints;
+    private int waypointIndex = 0;
+
+    private Transform target;
+
+    private void Start()
+    {
+        if (patrolWaypoints.Length != 0)
+        {
+            target = patrolWaypoints[0];
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (patrolWaypoints.Length != 0 && isAlive)
+        {
+            Vector3 dir = target.position - transform.position;
+            transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+            if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+            {
+                GetNextWaypoint();
+            }
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (waypointIndex >= patrolWaypoints.Length - 1)
+        {
+            waypointIndex = 0;
+        }
+        else
+        {
+            waypointIndex++;
+        }
+        target = patrolWaypoints[waypointIndex];
     }
 
     public void TakeBullet()
