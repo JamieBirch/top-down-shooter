6663e0f10452a514b68464e99477aab048987da4
JamieBirch <jamie.d.birch@gmail.com>
2024-07-01 12:54:47 +0200

    open doors by collision

--- Assets/scripts/Door.cs
+++ Assets/scripts/Door.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public bool isLocked;
+    private bool isOpen = false;
     public Rigidbody2D rb;
-    public GameObject rotationPoint;
+    // public GameObject rotationPoint;
+    
 
     public void Open()
     {
-        isLocked = false;
-        rotationPoint.transform.Rotate(0f, 0f, 90f);
-        rb.bodyType = RigidbodyType2D.Static;
+        if (!isOpen)
+        {
+            isOpen = true;
+            isLocked = false;
+            /*rotationPoint.*/transform.Rotate(0f, 0f, 90f);
+            rb.bodyType = RigidbodyType2D.Static;
+        }
+    }
+    
+    
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject colGameObject = col.gameObject;
+        if (colGameObject.TryGetComponent<Bullet>(out _) || colGameObject.CompareTag("Player"))
+        {
+            Open();
+        }
     }
 
 }
